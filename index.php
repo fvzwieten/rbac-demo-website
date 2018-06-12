1a9fcebdf787f2017e3269a5cc78b82a9cbc901c
fvzwieten <fvzwieten@gmail.com>
2018-06-12 08:43:31 +0200

    Update index.php
    color code webserver names based on webserver and auto refresh

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
   
   <div><h3>Website deployed using Ansible Tower</h3></div>
   <?php
-    echo "Served to you from: ", gethostname();
+    header("Refresh: 1; url=$page");
+    $fqdn = gethostname();
+    $pos = strpos($fqdn,'.');
+    $host = substr($fqdn, 0, $pos);
+    $domain = substr($fqdn,$pos);
+    echo "Served to you from: ";
+    if ($host = 'webserver1') {
+       echo '<span style="color:red;">', $host, '</span>';
+    } else {
+       echo '<span style="color:green;">', $host, '</span>';
+    }
+    echo $domain;
   ?>
 </body>
 </html>
