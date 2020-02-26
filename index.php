0f80bcaf94685425071c0ac8ff9b0e7f603aaaae
fvzwieten <fvanzwie@redhat.com>
2020-02-26 18:50:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,17 @@
     header("Refresh: 1; url=$page");
     $fqdn = gethostname();
     $pos = strpos($fqdn,'.');
-    $host = substr($fqdn, 0, $pos);
-    $domain = substr($fqdn,$pos);
+    $host = substr($fqdn,$pos);
     $last = substr($host,-1);
     if ($last == '1') {
         $color = 'Red';
     } else {
         $color = 'Blue';
     }
-  echo '<span style="color:', $color, ';">', $host, '</span>';
   echo '<div style="color:', $color, ';">';
   echo '<h1>Hello ACME!</h1>';
-  echo '<h3>Website deployed using Ansible Tower</h3>';
-  echo 'Served to you from: <span>', $host, '</span>';
+  echo '<h2>Website deployed using Ansible Tower</h2>';
+  echo '<h3>Served to you from: <span>', $host, '</span></h3>';
   echo '</div>'
   ?>
 </body>
