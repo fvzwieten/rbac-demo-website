8da16638d4bcaf091a7569950ead18d15df6c691
fvzwieten <fvanzwie@redhat.com>
2022-01-17 13:19:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     }
   echo '<div style="color:', $color, ';">';
   echo '<h1>Welcome @ ACME cop</h1>';
-  echo '<h2>Website deployed using Ansible Automation Platform</h2>';
+  echo '<h2>Website deployed using Ansible Automation Plat</h2>';
   echo '<h3>Served to you from: <span>', $host, '</span></h3>';
   echo '</div>'
   ?>
