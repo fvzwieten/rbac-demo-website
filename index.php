bec35bed60f52fadd244850003045c3c2e73a667
fvzwieten <fvanzwie@redhat.com>
2022-07-06 12:02:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <body>
   <?php
     header("Refresh: 1; url=$page");
+    $website_config = parse_ini_file("/etc/webserver.conf");
     $fqdn = gethostname();
-    $pos = strpos($fqdn,'.');
-    $host = substr($fqdn,$pos);
-    $last = substr($host,-1);
-    if ($last == '1') {
-        $color = 'Red';
-    } else {
-        $color = 'Blue';
-    }
-  echo '<div style="color:', $color, ';">';
-  echo '<h1>Welcome @ Demo company</h1>';
-  echo '<h2>Website deployed using Ansible Automation Platform</h2>';
-  echo '<h3>Served to you from: <span>', $host, '</span></h3>';
-  echo '</div>'
+    $version = $website_config['version'];
+    $serial = $website_config['serial'];
+    $colors = ["red","green","blue","purple","teal","fuchsia","coral","olive","navy"];
+    $color = $colors[ ($version % 9) + $serial ];
+    echo '<div style="color:', $color, ';">';
+
+    echo '<h1>Welcome @ Demo company</h1>';
+
+    echo '<h2>Website deployed using Ansible Automation Platform</h2>';
+
+    echo '<h3>Served to you from: <span>', $fqdn, ' (version: ', $version, ')</span></h3>';
+
+    echo '</div>';
   ?>
 </body>
 </html>
-
