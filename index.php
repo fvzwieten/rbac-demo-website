0495d4eb73605809e81fb7b3fca21051604ef74a
fvzwieten <fvanzwie@redhat.com>
2022-07-06 16:26:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     $version = $website_config['version'];
     $serial = $website_config['serial'];
     $colors = ["red","green","blue","purple","teal","fuchsia","coral","olive","navy"];
-    $color = $colors[ ($version % 9) + $serial ];
+    $color = $colors[ ($version % 6) + ( $serial * 2 ) ];
     echo '<div style="color:', $color, ';">';
 
     echo '<h1>Welcome Acme Coorporation</h1>';
